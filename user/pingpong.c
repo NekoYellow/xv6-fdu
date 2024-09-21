4b2573e9f6d3831319542e7d81ed05f5c64c74a6
Yueyuan Huang <harvyhyy@gmail.com>
2024-09-22 02:46:12 +0800

    Update pingpong: uses 2 pipes instead

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,38 +5,40 @@
 int main(int argc, char *argv[]) {
   int ppid, cpid;
   char buf[2], *info = "a";
-  int p[2];
-  pipe(p);
+  int p2c[2], c2p[2];
+  pipe(p2c); pipe(c2p);
 
   ppid = getpid();
 
   if ((cpid = fork()) == 0) { // child
     cpid = getpid();
-    if (read(p[0], buf, 1) != 1) {
+    close(p2c[1]);
+    if (read(p2c[0], buf, 1) != 1) {
       fprintf(2, "Failed to read in child\n");
       exit(1);
     }
-    close(p[0]);
     printf("%d: received ping from pid %d\n", cpid, ppid);
-    if (write(p[1], buf, 1) != 1) {
+    close(c2p[0]);
+    if (write(c2p[1], buf, 1) != 1) {
       fprintf(2, "Failed to write in child\n");
       exit(1);
     }
-    close(p[1]);
+    close(c2p[1]);
     exit(0);
   }
   // parent
-  if (write(p[1], info, 1) != 1) {
+  close(p2c[0]);
+  if (write(p2c[1], info, 1) != 1) {
     fprintf(2, "Failed to write in parent\n");
     exit(1);
   }
-  close(p[1]);
+  close(p2c[1]);
   wait(0);
-  if (read(p[0], buf, 1) != 1) {
+  close(c2p[1]);
+  if (read(c2p[0], buf, 1) != 1) {
     fprintf(2, "Failed to read in parent\n");
     exit(1);
   }
   printf("%d: received pong from pid %d\n", ppid, cpid);
-  close(p[0]);
   exit(0);
 }
