05ddf693122673d76c6f45cd8b19b5a07061f148
Yueyuan Huang <harvyhyy@gmail.com>
2024-12-21 13:24:46 +0800

    Update uidtest

--- user/uidtest.c
+++ user/uidtest.c
@@ -3,6 +3,7 @@
 // the uid printed is also the shell's.
 
 #include "kernel/types.h"
+#include "Kernel/param.h"
 #include "user/user.h"
 
 int
@@ -12,7 +13,36 @@ main(int argc, char *argv[])
 
   pid = getpid();
   uid = getuid(pid);
-  printf("#%d: %d\n", pid, uid);
+  
+  if (fork() == 0) {
+    if (getuid(getpid()) != uid) {
+      printf("Error: child process did not inherit uid\n");
+      exit(1);
+    }
+    exit(0);
+  } else {
+    wait(0);
+  }
+
+  uid ^= 1;
+  if (setuid(pid, uid, PWD) < 0) {
+    printf("Error: setuid failed\n");
+    exit(1);
+  }
+  if (uid != getuid(pid)) {
+    printf("Error: setuid did not work\n");
+    exit(1);
+  }
+
+  if (fork() == 0) {
+    if (getuid(getpid()) != uid) {
+      printf("Error: child process did not inherit uid\n");
+      exit(1);
+    }
+    exit(0);
+  }
+
+  printf("Everything ok if this is the only output\n");
 
   exit(0);
 }
